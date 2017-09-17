aa31ba7ad76835b0d5b3ec4dd0e2202f39002f8a
Denis Papathanasiou <denis@papathanasiou.org>
2017-09-17 11:11:12 -0400

    Added a second index parameter to a variant of the first function, to return an Int instead of Bool

--- how-to-describe-and-evaluate-computer-algorithms/LinearSearch.hs
+++ how-to-describe-and-evaluate-computer-algorithms/LinearSearch.hs
@@ -1,5 +1,6 @@
 module LinearSearch(
-myElem
+myElem,
+searchWithIndex
 ) where
 
 myElem :: (Eq a) => a -> [a] -> Bool
@@ -7,3 +8,9 @@ myElem a [] = False
 myElem a (x:xs)
      | a == x    = True
      | otherwise = a `myElem` xs
+
+searchWithIndex :: (Eq a) => a -> [a] -> Int -> Int
+searchWithIndex a [] _ = -1
+searchWithIndex a (x:xs) i
+              | a == x    = i
+              | otherwise = searchWithIndex a xs (i + 1)
