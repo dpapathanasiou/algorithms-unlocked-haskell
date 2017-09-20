589ca088fe75def3314f3dbbc9c2123a4cc826c8
Denis Papathanasiou <denis@papathanasiou.org>
2017-09-20 12:28:16 -0400

    Realized that procedure is 1-indexed, so updated initial condition accordingly, and removed extra guard

--- algorithms-for-sorting-and-searching/BinarySearch.hs
+++ algorithms-for-sorting-and-searching/BinarySearch.hs
@@ -4,12 +4,11 @@ search
 
 recursiveBinarySearch :: (Ord a) => [a] -> Int -> Int -> a -> Int
 recursiveBinarySearch a p r x
-                    | p > r           = -1
-                    | q >= (length a) = -1
+                    | p > r       = -1
                     | a !! q == x = q
                     | a !! q  > x = recursiveBinarySearch a p (q - 1) x
                     | otherwise   = recursiveBinarySearch a (q + 1) r x
                     where q = (p + r) `div` 2
 
 search :: (Ord a) => [a] -> a -> Int
-search a x = recursiveBinarySearch a 0 (length a) x
+search a x = recursiveBinarySearch a 0 ((length a) - 1) x
