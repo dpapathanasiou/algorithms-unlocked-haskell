0be298f7fd3e555cee25583336e22afb41a40de4
Denis Papathanasiou <denis@papathanasiou.org>
2017-09-21 05:06:24 -0400

    First attempt at selection sort (buggy)

---
+++ algorithms-for-sorting-and-searching/SelectionSort.hs
@@ -0,0 +1,20 @@
+module SelectionSort (
+selectionSort
+) where
+
+swap :: (Ord a) => [a] -> Int -> Int -> [a]
+swap a i j = let h = take i a
+                 m = take (j - i - 1) (drop (i + 1) a)
+                 t = drop (j + 1) a
+             in h ++ [a !! j] ++ m ++ [a !! i] ++ t
+
+findSmallest :: (Ord a) => [a] -> Int -> Int -> Int
+findSmallest a i j
+           | j == (length a) = i
+           | a !! i > a !! j = findSmallest a j (j + 1)
+           | otherwise       = findSmallest a i (j + 1)
+
+selectionSort :: (Ord a) => [a] -> Int -> [a]
+selectionSort a i
+             | i == (length a) = a
+             | otherwise = selectionSort (swap a i (findSmallest a i (i + 1))) (i + 1)
