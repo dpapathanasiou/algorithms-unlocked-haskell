4fde0d77d40ecbb88c15c84862e9e07fca1460de
Denis Papathanasiou <denis@papathanasiou.org>
2017-09-21 13:12:41 -0400

    Corrected logic in findSmallest, and added guard in swap to prevent infinite looping when called by selectionSort

--- algorithms-for-sorting-and-searching/SelectionSort.hs
+++ algorithms-for-sorting-and-searching/SelectionSort.hs
@@ -3,15 +3,17 @@ selectionSort
 ) where
 
 swap :: (Ord a) => [a] -> Int -> Int -> [a]
-swap a i j = let h = take i a
-                 m = take (j - i - 1) (drop (i + 1) a)
-                 t = drop (j + 1) a
-             in h ++ [a !! j] ++ m ++ [a !! i] ++ t
+swap a i j
+   | i >= j = a
+   | otherwise = let h = take i a
+                     m = take (j - i - 1) (drop (i + 1) a)
+                     t = drop (j + 1) a
+                 in h ++ [a !! j] ++ m ++ [a !! i] ++ t
 
 findSmallest :: (Ord a) => [a] -> Int -> Int -> Int
 findSmallest a i j
            | j == (length a) = i
-           | a !! i > a !! j = findSmallest a j (j + 1)
+           | a !! j < a !! i = findSmallest a j (j + 1)
            | otherwise       = findSmallest a i (j + 1)
 
 selectionSort :: (Ord a) => [a] -> Int -> [a]
