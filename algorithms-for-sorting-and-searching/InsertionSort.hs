96dc83e2363871edd59aa677ef6f3bff3fa04af2
Denis Papathanasiou <denis@papathanasiou.org>
2017-09-22 12:04:26 -0400

    Implemented insertion sort

---
+++ algorithms-for-sorting-and-searching/InsertionSort.hs
@@ -0,0 +1,17 @@
+replace :: (Ord a) => [a] -> Int -> a -> [a]
+replace a i x = let h = take i a
+                    t = drop (i + 1) a
+                in h ++ [x] ++ t
+
+decrementInner :: (Ord a) => [a] -> Int -> a -> ([a], Int)
+decrementInner a j k
+             | j == -1    = (a, j)
+             | a !! j < k = (a, j)
+             | otherwise  = decrementInner (replace a (j + 1) (a !! j)) (j - 1) k
+
+insertionSort :: (Ord a) => [a] -> Int -> [a]
+insertionSort a i
+            | i == (length a) = a
+            | otherwise = let k = a !! i
+                              (a', j) = decrementInner a (i - 1) k
+                          in insertionSort (replace a' (j + 1) k) (i + 1)
