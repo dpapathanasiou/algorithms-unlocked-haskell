859be6f5f8d765ea303ba33e3918e4e711e543f7
Denis Papathanasiou <denis@papathanasiou.org>
2017-09-26 07:26:18 -0400

    In the same way that merge could be defined recursively w/o explicit p, q, r parameters (since they are properties of the length of the list), redefined mergeSort to work on both halves

--- algorithms-for-sorting-and-searching/MergeSort.hs
+++ algorithms-for-sorting-and-searching/MergeSort.hs
@@ -1,8 +1,3 @@
-slice :: (Ord a) => [a] -> Int -> Int -> [a]
-slice a i j
-    | i > j = a
-    | otherwise = take (j - i + 1) (drop i a)
-
 merge' :: (Ord a) => [a] -> [a] -> [a] -> [a]
 merge' a [] c = a ++ c
 merge' a b [] = a ++ b
@@ -10,11 +5,8 @@ merge' a (bh:bt) (ch:ct)
      | bh <= ch  = merge' (a ++ [bh]) bt (ch:ct)
      | otherwise = merge' (a ++ [ch]) (bh:bt) ct
 
-merge :: (Ord a) => [a] -> Int -> Int -> Int -> [a]
-merge a p q r = merge' [] (slice a p q) (slice a (q + 1) r)
-
-mergeSort :: (Ord a) => [a] -> Int -> Int -> [a]
-mergeSort a p r
-        | p >= r = a
-        | otherwise = merge (mergeSort (mergeSort a p q) (q + 1) r) p q r
-        where q = (p + r) `div` 2
+mergeSort :: (Ord a) => [a] -> [a]
+mergeSort a
+        | (length a) < 2 = a
+        | otherwise = merge' [] (mergeSort (take mid a)) (mergeSort (drop mid a))
+        where mid = (length a) `div` 2
