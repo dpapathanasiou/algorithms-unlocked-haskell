74a2982f30c1d51b8e4ab5edae23bd7c43c20505
Denis Papathanasiou <denis@papathanasiou.org>
2017-09-26 07:27:53 -0400

    Renamed merge' to merge and added module

--- algorithms-for-sorting-and-searching/MergeSort.hs
+++ algorithms-for-sorting-and-searching/MergeSort.hs
@@ -1,12 +1,16 @@
-merge' :: (Ord a) => [a] -> [a] -> [a] -> [a]
-merge' a [] c = a ++ c
-merge' a b [] = a ++ b
-merge' a (bh:bt) (ch:ct)
-     | bh <= ch  = merge' (a ++ [bh]) bt (ch:ct)
-     | otherwise = merge' (a ++ [ch]) (bh:bt) ct
+module MergeSort (
+mergeSort
+) where
+
+merge :: (Ord a) => [a] -> [a] -> [a] -> [a]
+merge a [] c = a ++ c
+merge a b [] = a ++ b
+merge a (bh:bt) (ch:ct)
+     | bh <= ch  = merge (a ++ [bh]) bt (ch:ct)
+     | otherwise = merge (a ++ [ch]) (bh:bt) ct
 
 mergeSort :: (Ord a) => [a] -> [a]
 mergeSort a
         | (length a) < 2 = a
-        | otherwise = merge' [] (mergeSort (take mid a)) (mergeSort (drop mid a))
+        | otherwise = merge [] (mergeSort (take mid a)) (mergeSort (drop mid a))
         where mid = (length a) `div` 2
