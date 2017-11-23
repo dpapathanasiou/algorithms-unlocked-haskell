a7f1a00bf691e3f10bca59984a1456b1fc5c5653
Denis Papathanasiou <denis@papathanasiou.org>
2017-11-23 18:13:19 -0500

    First unit test, using Test.HUnit

---
+++ directed-acyclic-graphs/TopologicalSort2DTest.hs
@@ -0,0 +1,26 @@
+import Test.HUnit
+import Data.Array
+import TopologicalSort2D
+
+-- perform topological sort on a valid graph definition
+example = makeGraph [[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+expected = [0,1,8,2,9,3,4,5,6,10,7,11,12,13]
+
+graph = case example of
+  Nothing -> array ((0,0),(0,0)) [((0,0),0)]
+  Just  x -> x
+
+test1 = TestCase (assertEqual "example graph topological sort" expected (sort(graph)))
+
+-- confirm invalid graphs are detected
+invalid = makeGraph [[0,0,1], [0,0]]
+
+test2 = TestCase (assertEqual "invalid graph is Nothing" Nothing invalid)
+
+-- etc.: add more test cases here
+-- ...
+
+
+-- define the list of test cases
+unitTests = TestList [test1, test2]
